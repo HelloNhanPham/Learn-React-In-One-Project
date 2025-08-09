80decb1f4bb36a5644b7898e053a7d61f9d00851
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-09 22:12:26 +0700

    Refactor API service to use axios for HTTP requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,25 @@
+import axios from "axios";
+
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(
-    `${BASE_URL}/movie/popular?api_key=${process.env.API_KEY}`
+  const response = await axios.get(
+    `${BASE_URL}/movie/popular?language=en-US&page=1`,
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+      },
+    }
   );
-  const data = await response.json();
-  return data.results;
+  return response.data.results;
 };
 
 export const searchMovies = async (query) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${BASE_URL}/search/movie?api_key=${
-      process.env.API_KEY
+      import.meta.env.VITE_API_KEY
     }&query=${encodeURIComponent(query)}`
   );
-  const data = await response.json();
-  return data.results;
+  return response.data.results;
 };
